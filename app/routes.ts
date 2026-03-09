560b2b46b54172d0b5162b1c8c290d26184d34ab
Alex Bruh <53874345+Alexbruh14@users.noreply.github.com>
2026-03-08 23:13:31 -0700

    Final working version

--- app/routes.ts
+++ app/routes.ts
@@ -5,15 +5,18 @@ import Essays from "./pages/Essays";
 import BookReviews from "./pages/BookReviews";
 import NotFound from "./pages/NotFound";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Root,
-    children: [
-      { index: true, Component: Home },
-      { path: "essays", Component: Essays },
-      { path: "book-reviews", Component: BookReviews },
-      { path: "*", Component: NotFound },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Root,
+      children: [
+        { index: true, Component: Home },
+        { path: "essays", Component: Essays },
+        { path: "book-reviews", Component: BookReviews },
+        { path: "*", Component: NotFound },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
